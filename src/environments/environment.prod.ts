73e02b3a2b5669bca09fcc0d1a973c1ab62c49a2
swarup <swarupbhol34@gmail.com>
2026-03-03 23:55:01 +0530

    url changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://bookurbarber-backend-production.up.railway.app/api',
-  appName: 'BookurBarber',
-  appUrl: 'https://bookurbarber-production.up.railway.app/'
+  appName: 'BookurBarber'
 };
